8b8318fa2f40c75a948249d58d4cc9ae90e3f42b
Andrade <andrade.nicolas.melo@outlook.com>
2025-04-07 20:20:35 -0300

    Adicionar arquivos de projeto.

---
+++ jogoDaForcaConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace jogoDaForcaConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" ___________        ");
+            Console.WriteLine(" |/        |        ");
+            Console.WriteLine(" |                  ");
+            Console.WriteLine(" |                  ");
+            Console.WriteLine(" |                  ");
+            Console.WriteLine(" |                  ");
+            Console.WriteLine(" |                  ");
+            Console.WriteLine(" |                  ");
+            Console.WriteLine("_|____              ");
+        }
+    }
+}
